1bcc76fbca0e10cc1410e4cd7ccbd05f763324cc
Pierina Lopez <r0913865@student.thomasmore.be>
2024-06-04 16:28:36 +0200

    Asp.NET MVC PART 1

---
+++ www/MotoGP/MotoGP/Controllers/InfoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotoGP.Models;
+
+namespace MotoGP.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListRaces() 
+        {
+            int BannerNr = 0;
+            ViewData["BannerNr"] = BannerNr;
+            return View();
+        
+        }
+
+        public IActionResult BuildMap()
+        {
+            int BannerNr = 0;
+            ViewData["BannerNr"] = BannerNr;
+            var race1 = new Race() { RaceID = 1, X = 517, Y = 19, Name = "Assen" };
+            var race2 = new Race() { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" };
+            var race3 = new Race() { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" };
+            var races = new List<Race>() { race1, race2, race3 };
+            ViewData["races"] = races;
+            return View();
+        }
+    }
+}
---
+++ www/MotoGP/MotoGP/Controllers/ShopController.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MotoGP.Data;
+using MotoGP.Models;
+using System.Net.Sockets;
+using System;
+
+namespace MotoGP.Controllers
+{
+    public class ShopController : Controller
+    {
+     
+
+        
+        public IActionResult OrderTicket()
+        {
+            int BannerNr = 3;
+            ViewData["BannerNr"] = BannerNr;
+            
+            return View();
+        }
+
+        public IActionResult ConfirmOrder()
+        { 
+            int BannerNr = 3;
+            ViewData["BannerNr"] = BannerNr;
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
